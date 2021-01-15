7a9292d1c2cc710a1ec9f6b2f9394bf409a0dfdd
Pedro Manfredi <barcelo.sk8@gmail.com>
2021-01-15 17:54:55 +0100

    Initial Commit

---
+++ OneLoginPod/OneLoginPod.h
@@ -0,0 +1,18 @@
+//
+//  OneLoginPod.h
+//  OneLoginPod
+//
+//  Created by Pedro Manfredi on 15/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OneLoginPod.
+FOUNDATION_EXPORT double OneLoginPodVersionNumber;
+
+//! Project version string for OneLoginPod.
+FOUNDATION_EXPORT const unsigned char OneLoginPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneLoginPod/PublicHeader.h>
+
+
